92a5e5e35f2e45c2e89e7c342eef03065cdccfda
chambalon <nehachambalonk@gmail.com>
2025-11-11 06:40:37 +0800

    c1

---
+++ filemanager.py
@@ -0,0 +1,15 @@
+import tkinter as tk
+import os
+import shutil
+
+
+#class FileManagerApp:
+
+
+if __name__ == '__main__':
+  # Main window
+  root = tk.Tk()
+  label = tk.Label(root, text="File Manager")
+  label.pack()
+  # Keep the window open by running the Tkinter event loop
+  #root.mainloop()
