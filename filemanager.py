9f7ac930020e7e4a8d590608da08358e2ec148d7
chambalon <nehachambalonk@gmail.com>
2025-11-11 23:02:17 +0800

    Initialized the file manager app

--- filemanager.py
+++ filemanager.py
@@ -1,15 +1,27 @@
 import tkinter as tk
 import os
 import shutil
+from tkinter import filedialog, messagebox
 
 
-#class FileManagerApp:
+class FileManagerApp:
+  def __init__(self,root):
+    self.root = root
+    self.root.title("File Manager")
+    # Initial size of the window
+    self.root.geometry("600x400")
+    self.create_widgets()
+    #self.update_files()
+
+  def create_widgets(self):
+    self.label1 = tk.Label(self.root, text="path")
+    self.label1.pack()
+    
 
 
 if __name__ == '__main__':
   # Main window
   root = tk.Tk()
-  label = tk.Label(root, text="File Manager")
-  label.pack()
+  app = FileManagerApp(root)
   # Keep the window open by running the Tkinter event loop
-  #root.mainloop()
+  root.mainloop()
